3951427f2acd8c1d320111d1b9d199b6acb14d4c
Gloryjeong <vles153@naver.com>
2022-07-12 10:51:03 +0900

    7월 12일

---
+++ step12_JPA_Mapping/src/main/java/step06/entity/Member.java
@@ -0,0 +1,45 @@
+package step06.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Entity
+@ToString(exclude = {"order"})
+public class Member {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name="member_id")
+   private Long id;
+   
+   @Column(length = 20)
+   private String name;
+   
+   private Integer age;
+   
+   
+//   @OneToMany(mappedBy = "member", cascade = {CascadeType.REMOVE})
+   @OneToMany(mappedBy = "member")
+   private List<Order> order = new ArrayList<Order>(); 
+   
+   
+
+   
+}
